b6e900792fcb7554c82126bb250c09299ca7095c
venkatasaiyadavavula-cmd <venkatasaiyadavavula@gmail.com>
2026-04-30 11:25:58 +0530

    Update cart.module.ts

--- backend/src/modules/cart/cart.module.ts
+++ backend/src/modules/cart/cart.module.ts
@@ -1,12 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { RedisModule } from '@nestjs-modules/ioredis';
 import { CartController } from './cart.controller';
 import { CartService } from './cart.service';
 import { Product } from '../../core/entities/product.entity';
 import { Shop } from '../../core/entities/shop.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product, Shop])],
+  imports: [
+    TypeOrmModule.forFeature([Product, Shop]),
+    RedisModule.forRoot({
+      type: 'single',
+      url: 'redis://localhost:6379',
+    }),
+  ],
   controllers: [CartController],
   providers: [CartService],
   exports: [CartService],
