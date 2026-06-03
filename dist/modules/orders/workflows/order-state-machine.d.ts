3d0c81041b36d3dde01100e624f0330e7cd2593f
venkatasaiyadavavula-cmd <avulavenkatasaiyadav123@gmail.com>
2026-06-03 09:05:55 +0000

    fix: remove node_modules and dist from tracking

--- dist/modules/orders/workflows/order-state-machine.d.ts
+++
@@ -1,6 +0,0 @@
-import { OrderStatus } from '../../../core/entities/order.entity';
-export declare class OrderStateMachine {
-    private readonly transitions;
-    canTransition(currentStatus: OrderStatus, targetStatus: OrderStatus): boolean;
-    getNextAllowedStatuses(currentStatus: OrderStatus): OrderStatus[];
-}
--- dist/modules/seller/dto/subscription-plan.dto.d.ts
+++
@@ -1,5 +0,0 @@
-import { SubscriptionPlan } from '../../../core/entities/subscription.entity';
-export declare class SubscribeDto {
-    plan: SubscriptionPlan;
-    autoRenew?: boolean;
-}
