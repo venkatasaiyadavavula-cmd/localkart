26a57b979d088ab3c1c23438563504f1fa4b3f4b
venkatasaiyadavavula-cmd <venkatasaiyadavavula@gmail.com>
2026-06-04 11:55:00 +0530

    Update notifications.module.ts

--- backend/src/modules/notifications/notifications.module.ts
+++ backend/src/modules/notifications/notifications.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 import { SmsService } from './sms.service';
 import { EmailService } from './email.service';
+import { WhatsappService } from './whatsapp.service';
 
 @Module({
-  providers: [NotificationsService, SmsService, EmailService],
-  exports: [NotificationsService, SmsService, EmailService],
+  providers: [NotificationsService, SmsService, EmailService, WhatsappService],
+  exports:   [NotificationsService, SmsService, EmailService, WhatsappService],
 })
 export class NotificationsModule {}
