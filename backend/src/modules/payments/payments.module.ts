5abdaa9f55ccc2587003d8aed42dc10f8c6c8e10
venkatasaiyadavavula-cmd <venkatasaiyadavavula@gmail.com>
2026-06-04 11:46:33 +0530

    Update payments.module.ts

--- backend/src/modules/payments/payments.module.ts
+++ backend/src/modules/payments/payments.module.ts
@@ -1,19 +1,28 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ScheduleModule } from '@nestjs/schedule';
 import { PaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
 import { WebhookController } from './webhook.controller';
+import { CommissionService } from './commission.service';
+import { CommissionController } from './commission.controller';
 import { Order } from '../../core/entities/order.entity';
 import { Transaction } from '../../core/entities/transaction.entity';
+import { CommissionBill } from '../../core/entities/commission-bill.entity';
+import { Shop } from '../../core/entities/shop.entity';
 import { OrdersModule } from '../orders/orders.module';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Order, Transaction]),
+    TypeOrmModule.forFeature([Order, Transaction, CommissionBill, Shop]),
     OrdersModule,
+    NotificationsModule,
+    ScheduleModule.forRoot(),
   ],
-  controllers: [PaymentsController, WebhookController],
-  providers: [PaymentsService],
-  exports: [PaymentsService],
+  controllers: [PaymentsController, WebhookController, CommissionController],
+  providers: [PaymentsService, CommissionService],
+  exports: [PaymentsService, CommissionService],
 })
 export class PaymentsModule {}
+
